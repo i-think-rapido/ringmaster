5fe6768149e0b339f6f43a26510c94b6ebae76ee
Romeo Disca <romeo.disca@gmail.com>
2020-09-13 04:44:01 +0200

    feat: is_empty

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,40 @@
+use crate::RingBox::Root;
+
+enum RingBox<T> {
+    Root{prev: usize, next: usize},
+    Item{prev: usize, next: usize, payload: T},
+}
+
+struct Ring<T> {
+    buffer: Vec<RingBox<T>>,
+}
+
+impl<T> Ring<T> {
+
+    fn new() -> Ring<T> {
+        Ring {
+            buffer: vec![Root {prev: 0, next: 0}],
+        }
+    }
+
+    fn is_empty(&self) -> bool {
+        if let Root{prev, next} = self.buffer[0] {
+            prev == next
+        }
+        else {
+            false
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
+
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn empty_ring() {
+        let ring = Ring::<u8>::new();
+        assert!(ring.is_empty());
     }
 }
